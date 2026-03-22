78d4bc38d4ae46d2b8ddcbe33c57d7a570d57227
soumyapriyadarshi99 <das.soumyapd@gmail.com>
2026-03-22 23:35:17 +0530

    feat: package updated

---
+++ dsa/src/dynamicProgramming/DPClimbingStairLeetcode70.java
@@ -0,0 +1,22 @@
+package dynamicProgramming;
+
+import java.util.Scanner;
+
+public class DPClimbingStairLeetcode70 {
+	public static void main (String [] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter no of stares: ");
+		int stare = sc.nextInt();
+		System.out.println(findStep(stare));
+	}
+	
+	public static int findStep(int n) {
+		if(n<=1) {
+			return 1;
+		}
+		int step1=findStep(n-1);
+		int step2=findStep(n-2);
+		return step1+step2;
+	}
+
+}
